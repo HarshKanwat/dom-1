4007a9d88732af8e288c9c8ac3fcb3eeeb494d73
HarshKanwat <harshkanwat2002@gmail.com>
2024-07-15 12:27:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function q() {
+    var a = document.getElementById("Firstname").value;
+    console.log(a);
+}
+function w() {
+    var b = document.getElementById("Middlename").value;
+    console.log(b);
+}
+function e() {
+    var c = document.getElementById("Lastname").value;
+    console.log(c);
+}
+function r() {
+    var d = document.getElementById("email").value;
+    console.log(d);
+}
+function t() {
+    var res = document.getElementById("password").value;
+    console.log(res);
+}
